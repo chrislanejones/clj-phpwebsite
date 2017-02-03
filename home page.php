28cfdc37f4ffe5e382b62b19984d7165f448c91a
Chris Lane Jones <chrislanejones@gmail.com>
2017-02-02 23:42:16 -0500

    Almost Completed Website

--- home page.php
+++ home page.php
@@ -55,43 +55,45 @@
             <div class="box">
               <h2><div class="hicon"><i class="fa fa-briefcase faa-wrench animated-hover fa-fw" aria-hidden="true"></div></i> Experiences</h2>
               <div class="job clearfix">
-                <div class="col-xs-3">
-                  <div class="where">Web.com - Jacksonville, FL</div>
-                  <div class="year">October 2015 - Present</div>
-                </div>
-                <div class="col-xs-9">
+
+                <div class="col-xs-12">
                   <div class="profession">Graphic and Web Designer</div>
+                  <div class="where">Web.com | October 2015 – Present | Jacksonville, Florida Area</div>
                   <div class="description">
-      • Build and maintain portfolio-grade websites using HTML, CSS and Javascript for clients.
-  <br>• Ensure that every site is mobile responsive by setting and adjusting breakpoints.
-  <br>• Save the company money by redesigning escalated client’s websites in a fraction of allotted time.
-  <br>• Cross-trained in store support and E-commerce and help clients add products with ease.
-  <br>• Help customers maximize traction on Google by bringing their SEO score up to 100%.
-  <br>• Brainstorm ideas and find tools that expedite the process and create an easier customer experience.
-  <br>• Upsell clients to products that better suit their individual needs.
-  <br>• Brainstorm ideas and find tools that expedite the process and create an easier customer experience.
-  <br>• Exceed expectations by completing more websites per hour before and after training.
-  <br>• Taught my trainer and several veteran employees, keyboard shortcuts during training.</div>
+  <br><i class="fa fa-check" aria-hidden="true"></i> Build and maintain portfolio-grade websites using HTML, CSS and Javascript for clients.
+  <br><i class="fa fa-check" aria-hidden="true"></i> Ensure that every site is mobile responsive by setting and adjusting breakpoints.
+  <br><i class="fa fa-check" aria-hidden="true"></i> Save the company money by redesigning escalated client’s websites in a fraction of allotted time.
+  <br><i class="fa fa-check" aria-hidden="true"></i> Cross-trained in store support and E-commerce and help clients add products with ease.
+  <br><i class="fa fa-check" aria-hidden="true"></i> Help customers maximize traction on Google by bringing their SEO score up to 100%.
+  <br><i class="fa fa-check" aria-hidden="true"></i> Brainstorm ideas and find tools that expedite the process and create an easier customer experience.
+  <br><i class="fa fa-check" aria-hidden="true"></i> Upsell clients to products that better suit their individual needs.
+  <br><i class="fa fa-check" aria-hidden="true"></i> Brainstorm ideas and find tools that expedite the process and create an easier customer experience.
+  <br><i class="fa fa-check" aria-hidden="true"></i> Exceed expectations by completing more websites per hour before and after training.
+  <br><i class="fa fa-check" aria-hidden="true"></i> Taught my trainer and several veteran employees, keyboard shortcuts during training.
+  <br>
+  <br><i class="fa fa-trophy fa-lg" aria-hidden="true"></i>Awards:
+  <br>
+  <br><i class="fa fa-star-o" aria-hidden="true"></i> Marketing Maestro for April 2016 - #1 product up-seller on a team of 60+ workers.
+  <br><i class="fa fa-star-o" aria-hidden="true"></i> Earned a WOW Wizard website design and kept a Quality Control score of 100% in March 2016.
+  <br><i class="fa fa-star-o" aria-hidden="true"></i> Achieved third place in February 2016 for most website mods completed in a team of over fifty.
+  <br><i class="fa fa-star-o" aria-hidden="true"></i> WOW Wizard for design two weeks in a row in November 2016.
+  <br><i class="fa fa-star-o" aria-hidden="true"></i> Received the most end-of-call surveys on my team in November 2016.
+  <br><i class="fa fa-star-o" aria-hidden="true"></i> Earned a Chili's gift card from management for customer compliment voicemail in December 2016.
+</div>
                 </div>
               </div>
               <div class="job clearfix">
-                <div class="col-xs-3">
-                  <div class="where">Chris Lane Jones Freelance - Jacksonville, FL</div>
-                  <div class="year">2013</div>
-                </div>
-                <div class="col-xs-9">
+                <div class="col-xs-12">
                   <div class="profession">Web Designer and Developer, SEO and Video Editor</div>
+                  <div class="where">Chris Lane Jones | June 2013 – Present | Jacksonville, Florida Area</div>
                   <div class="description">
-  • Freelance for major clients: Jacksonville Area Legal Aid, Mary Pippin Design, Rethreaded and Girls Inc.
-  <br>• Create responsive HTML5 websites using WordPress utilizing a hosted server, design site with Photoshop, and find keywords for SEO.
-  <br>• Modify preexisting WordPress templates designs by editing the PHP code and downloading the MySQL database to move sites to another server.
-  <br>• Create high-quality industry standard videos and professional websites for clients
-  <br>• Spread the word via Twitter and Facebook connections and raised awareness to a local event.
-  <br>• Proactively find prospects by attending several networking events monthly, and cold calling.
-  <br>• Build and maintain customer relationships by creating quality customer service and cold calling.
-  <br>
-  <br>
-  <br>
+  <br><i class="fa fa-check" aria-hidden="true"></i> Freelance for major clients: Jacksonville Area Legal Aid, Mary Pippin Design, Rethreaded and Girls Inc.
+  <br><i class="fa fa-check" aria-hidden="true"></i> Create responsive HTML5 websites using WordPress utilizing a hosted server, design site with Photoshop, and find keywords for SEO.
+  <br><i class="fa fa-check" aria-hidden="true"></i> Modify preexisting WordPress templates designs by editing the PHP code and downloading the MySQL database to move sites to another server.
+  <br><i class="fa fa-check" aria-hidden="true"></i> Create high-quality industry standard videos and professional websites for clients
+  <br><i class="fa fa-check" aria-hidden="true"></i> Spread the word via Twitter and Facebook connections and raised awareness to a local event.
+  <br><i class="fa fa-check" aria-hidden="true"></i>  Proactively find prospects by attending several networking events monthly, and cold calling.
+  <br><i class="fa fa-check" aria-hidden="true"></i>  Build and maintain customer relationships by creating quality customer service and cold calling.
 
 
 
@@ -120,18 +122,19 @@
               <div class="contact-item">
                 <div class="icon pull-left text-center"><span class="fa fa-twitter fa-fw"></span></div>
                 <div class="title pull-right">Twitter</div>
-                <div class="description pull-right">https://twitter.com/christvtweets</div>
-              </div>
-              <div class="contact-item">
-                <div class="icon pull-left text-center"><span class="fa fa-facebook fa-fw"></span></div>
-                <div class="title pull-right">Facebook</div>
-                <div class="description pull-right">https://www.facebook.com/chrislanejones</div>
+                <div class="description pull-right"><a href="https://twitter.com/cljwebdev" target="_blank">@cljwebdev</a></div>
               </div>
               <div class="contact-item">
                 <div class="icon pull-left text-center"><span class="fa fa-linkedin fa-fw"></span></div>
                 <div class="title pull-right">LinkedIn</div>
-                <div class="description pull-right">https://www.linkedin.com/in/chrislanejones</div>
+                <div class="description pull-right"><a href="https://www.linkedin.com/in/chrislanejones" target="_blank">https://www.linkedin.com/in/chrislanejones</a></div>
               </div>
+              <div class="contact-item">
+                <div class="icon pull-left text-center"><span class="fa fa-github fa-fw"></span></div>
+                <div class="title pull-right">GitHub</div>
+                <div class="description pull-right"><a href="https://github.com/chrislanejones" target="_blank">https://github.com/chrislanejones</a></div>
+              </div>
+
             </div>
             <!-- SKILLS -->
 
@@ -192,11 +195,21 @@
                   </div>
                 </div>
                 </div>
+
+                <div class="barWrapper">
+                 <span class="progressText"><B>WordPress</B></span>
+                <div class="progress">
+                  <div class="progress-bar" role="progressbar" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100">
+                      <span  class="popOver" data-toggle="tooltip" data-placement="top" title="55%"></span>
+                  </div>
+                </div>
+                </div>
+
                 <div class="barWrapper">
                  <span class="progressText"><B>MYSQL</B></span>
                 <div class="progress">
-                  <div class="progress-bar" role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100">
-                      <span  class="popOver" data-toggle="tooltip" data-placement="top" title="45%"></span>
+                  <div class="progress-bar" role="progressbar" aria-valuenow="35" aria-valuemin="0" aria-valuemax="100">
+                      <span  class="popOver" data-toggle="tooltip" data-placement="top" title="35%"></span>
                   </div>
                 </div>
                 </div>
@@ -221,12 +234,12 @@
             <div class="head-wrap shift-text">
 
               <div class="text-editor-wrap">
-                <div class="title-bar"><span class="title">chrislanejones@chrislanejones-Inspiron-13-7368: ~ </span>
+                <div class="title-bar"><span class="title">clj@chrislanejones-Inspiron-13-7368:</span>
   </div><div>
 
 
                 <div class="text-body">
-                   <span id="typed">chrislanejones@chrislanejones-Inspiron-13-7368: $ sudo lshw
+                   <span id="typed">clj@chrislanejones-Inspiron-13-7368: $ sudo lshw
 
   </br>Primary OS: Ubuntu Gnome Linux
   </br>Primary IDE: Atom
@@ -234,6 +247,7 @@
   </br>Web Host Type: VPS Debian Droplet
   </br>Local Host: LAMP stack
   </br>PHP Editor: PHP Storm
+</br>Container Software: Docker
 
   </button></span>
 
--- includes/navbar-search.php
+++ includes/navbar-search.php
@@ -14,7 +14,8 @@ Then you can insert the Search Widget into the sidebar.
 	           <ul class="nav navbar-nav navbar-right social">
 	               <li><a href="https://twitter.com/cljwebdev" target="_blank"><i class="fa fa-lg fa-twitter"></i></a></li>
 	               <li><a href="https://www.linkedin.com/in/chrislanejones" target="_blank"><i class="fa fa-lg fa-linkedin"></i></a></li>
-	               <li><a href="https://codepen.io/chrislanejones/" target="_blank"><i class="fa fa-lg fa-codepen"></i></a></li>
+	               <li><a href="https://codepen.io/chrislanejones" target="_blank"><i class="fa fa-lg fa-codepen"></i></a></li>
 	               <li><a href="https://github.com/chrislanejones" target="_blank"><i class="fa fa-lg fa-github"></i></a></li>
+                       <li><a href="https://www.freecodecamp.com/chrislanejones" target="_blank"><i class="fa fa-lg fa-free-code-camp"></i></a></li>
 
 	</button></a></li>
