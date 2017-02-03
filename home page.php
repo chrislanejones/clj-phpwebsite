85dc4fc514e8d67fb093bfe4e0388030de04f85f
Chris Lane Jones <chrislanejones@gmail.com>
2017-02-02 20:07:08 -0500

    Adding files

---
+++ home page.php
@@ -0,0 +1,277 @@
+<?php
+    /*
+     Template Name: home page
+    */
+?>
+
+<?php get_template_part('includes/header'); ?>
+
+<div class="container">
+  <div class="row">
+<div class="pageheight">
+
+
+    <div class="col-xs-12 col-sm-11">
+      <div id="content" role="main">
+        <?php get_template_part('includes/loops/content', 'page'); ?>
+
+        <div class="row">
+          <div class="col-xs-12 col-sm-7">
+            <!-- ABOUT ME -->
+            <div class="box">
+
+
+            </div>
+
+
+            <!-- EDUCATION -->
+            <div class="box">
+              <h2><div class="hicon"><i class="fa fa-graduation-cap faa-bounce animated-hover fa-fw" aria-hidden="true"></div></i> Education & Web History</h2>
+              <ul id="education" class="clearfix">
+                <li>
+                  <div class="year pull-left">2012</div>
+                  <div class="description pull-right">
+                    <h3>Built my first website for my TV/Radio portfolio</h3>
+                    <p>While my other classmates used a free blog, I went out and purchased my own website host, used a WordPress theme and even styled and learned some PHP.</p>
+                  </div>
+                <li>
+                  <div class="year pull-left">2013</div>
+                  <div class="description pull-right">
+                    <h3>Designed a website for a Non-Profit</h3>
+                    <p>I volunteered and built a WordPress site for a non-profit student produced, film award show.</p>
+                  </div>
+                </li>
+                <li>
+                  <div class="year pull-left">2013</div>
+                  <div class="description pull-right">
+                    <h3>Graduated from University of North Florida</h3>
+                    <p>Graduated with a bachelor's in communication with concentration in Multimedia Journalism & Production.</p>
+                  </div>
+                </li>
+
+              </ul>
+            </div>
+            <!-- EXPERIENCES -->
+            <div class="box">
+              <h2><div class="hicon"><i class="fa fa-briefcase faa-wrench animated-hover fa-fw" aria-hidden="true"></div></i> Experiences</h2>
+              <div class="job clearfix">
+                <div class="col-xs-3">
+                  <div class="where">Web.com - Jacksonville, FL</div>
+                  <div class="year">October 2015 - Present</div>
+                </div>
+                <div class="col-xs-9">
+                  <div class="profession">Graphic and Web Designer</div>
+                  <div class="description">
+      • Build and maintain portfolio-grade websites using HTML, CSS and Javascript for clients.
+  <br>• Ensure that every site is mobile responsive by setting and adjusting breakpoints.
+  <br>• Save the company money by redesigning escalated client’s websites in a fraction of allotted time.
+  <br>• Cross-trained in store support and E-commerce and help clients add products with ease.
+  <br>• Help customers maximize traction on Google by bringing their SEO score up to 100%.
+  <br>• Brainstorm ideas and find tools that expedite the process and create an easier customer experience.
+  <br>• Upsell clients to products that better suit their individual needs.
+  <br>• Brainstorm ideas and find tools that expedite the process and create an easier customer experience.
+  <br>• Exceed expectations by completing more websites per hour before and after training.
+  <br>• Taught my trainer and several veteran employees, keyboard shortcuts during training.</div>
+                </div>
+              </div>
+              <div class="job clearfix">
+                <div class="col-xs-3">
+                  <div class="where">Chris Lane Jones Freelance - Jacksonville, FL</div>
+                  <div class="year">2013</div>
+                </div>
+                <div class="col-xs-9">
+                  <div class="profession">Web Designer and Developer, SEO and Video Editor</div>
+                  <div class="description">
+  • Freelance for major clients: Jacksonville Area Legal Aid, Mary Pippin Design, Rethreaded and Girls Inc.
+  <br>• Create responsive HTML5 websites using WordPress utilizing a hosted server, design site with Photoshop, and find keywords for SEO.
+  <br>• Modify preexisting WordPress templates designs by editing the PHP code and downloading the MySQL database to move sites to another server.
+  <br>• Create high-quality industry standard videos and professional websites for clients
+  <br>• Spread the word via Twitter and Facebook connections and raised awareness to a local event.
+  <br>• Proactively find prospects by attending several networking events monthly, and cold calling.
+  <br>• Build and maintain customer relationships by creating quality customer service and cold calling.
+  <br>
+  <br>
+  <br>
+
+
+
+
+  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+          <div class="col-xs-12 col-sm-5">
+            <!-- CONTACT -->
+            <div class="box clearfix">
+              <h2></h2>
+
+              <h2><div class="hicon"><i class="fa fa-phone faa-ring animated-hover fa-fw" aria-hidden="true"></div></i> Contact</h2>
+              <div class="contact-item">
+                <div class="icon pull-left text-center"><span class="fa fa-phone fa-fw"></span></div>
+                <div class="title only pull-right">1 (904) 327-3284</div>
+              </div>
+              <div class="contact-item">
+                <div class="icon pull-left text-center"><span class="fa fa-envelope fa-fw"></span></div>
+                <div class="title only pull-right">chrislanejones@gmail.com</div>
+              </div>
+              <div class="contact-item">
+                <div class="icon pull-left text-center"><span class="fa fa-twitter fa-fw"></span></div>
+                <div class="title pull-right">Twitter</div>
+                <div class="description pull-right">https://twitter.com/christvtweets</div>
+              </div>
+              <div class="contact-item">
+                <div class="icon pull-left text-center"><span class="fa fa-facebook fa-fw"></span></div>
+                <div class="title pull-right">Facebook</div>
+                <div class="description pull-right">https://www.facebook.com/chrislanejones</div>
+              </div>
+              <div class="contact-item">
+                <div class="icon pull-left text-center"><span class="fa fa-linkedin fa-fw"></span></div>
+                <div class="title pull-right">LinkedIn</div>
+                <div class="description pull-right">https://www.linkedin.com/in/chrislanejones</div>
+              </div>
+            </div>
+            <!-- SKILLS -->
+
+
+
+
+
+            <div class="box">
+              <h2><div class="hicon"><i class="fa fa-bar-chart faa-tada animated-hover fa-fw" aria-hidden="true"></div></i> Skills</h2>
+              <div class="skills">
+                <div class="barWrapper">
+                 <span class="progressText"><B>HTML5</B></span>
+                <div class="progress">
+                  <div class="progress-bar" role="progressbar" aria-valuenow="95" aria-valuemin="0" aria-valuemax="100" >
+                        <span  class="popOver" data-toggle="tooltip" data-placement="top" title="95%"> </span>
+                </div>
+                </div>
+
+                <div class="barWrapper">
+                 <span class="progressText"><B>CSS3</B></span>
+                <div class="progress ">
+                  <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="10" aria-valuemax="100" style="">
+                     <span  class="popOver" data-toggle="tooltip" data-placement="top" title="90%"> </span>
+                  </div>
+
+                </div>
+                </div>
+
+                <div class="barWrapper">
+                 <span class="progressText"><B>BOOTSTRAP</B></span>
+                <div class="progress">
+                  <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100">
+                     <span  class="popOver" data-toggle="tooltip" data-placement="top" title="80%"> </span>
+                  </div>
+                </div>
+                </div>
+                <div class="barWrapper">
+                 <span class="progressText"><B>JQUERY</B></span>
+                <div class="progress">
+                  <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
+                         <span  class="popOver" data-toggle="tooltip" data-placement="top" title="75%"> </span>
+                  </div>
+                </div>
+                </div>
+                <div class="barWrapper">
+                 <span class="progressText"><B>JAVASCRIPT</B></span>
+                <div class="progress">
+                  <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
+                         <span  class="popOver" data-toggle="tooltip" data-placement="top" title="75%"> </span>
+                  </div>
+                </div>
+                </div>
+                  <div class="barWrapper">
+                 <span class="progressText"><B>PHP</B></span>
+                <div class="progress">
+                  <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
+                      <span  class="popOver" data-toggle="tooltip" data-placement="top" title="75%"> </span>
+                  </div>
+                </div>
+                </div>
+                <div class="barWrapper">
+                 <span class="progressText"><B>MYSQL</B></span>
+                <div class="progress">
+                  <div class="progress-bar" role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100">
+                      <span  class="popOver" data-toggle="tooltip" data-placement="top" title="45%"></span>
+                  </div>
+                </div>
+                </div>
+
+                <div class="barWrapper">
+                 <span class="progressText"><B>ANGULAR</B></span>
+                <div class="progress">
+                   <div class="progress">
+                     <div class="progress-bar" role="progressbar" aria-valuenow="3" aria-valuemin="0" aria-valuemax="100">
+                  </div>
+                </div>
+                </div>
+
+
+
+
+
+
+          <div class="header">
+            <h2><div class="hicon"><i class="fa fa-terminal faa-flash animated-hover fa-fw" aria-hidden="true"></div></i> Terminal</h2>
+
+            <div class="head-wrap shift-text">
+
+              <div class="text-editor-wrap">
+                <div class="title-bar"><span class="title">chrislanejones@chrislanejones-Inspiron-13-7368: ~ </span>
+  </div><div>
+
+
+                <div class="text-body">
+                   <span id="typed">chrislanejones@chrislanejones-Inspiron-13-7368: $ sudo lshw
+
+  </br>Primary OS: Ubuntu Gnome Linux
+  </br>Primary IDE: Atom
+  </br>Text Editors: Nano, Gedit
+  </br>Web Host Type: VPS Debian Droplet
+  </br>Local Host: LAMP stack
+  </br>PHP Editor: PHP Storm
+
+  </button></span>
+
+
+
+
+  </span><span class="typed-cursor"></span>
+
+                </div>
+              </div>
+
+            </div>
+            <!-- HOBBIES -->
+            <div class="box">
+              <h2></h2>
+              <h2><div class="hicon"><i class="fa fa-paper-plane faa-passing animated-hover fa-fw" aria-hidden="true"></div></i> Hobbies and Interest</h2>
+              <div class="hobby">Javascript</div>
+              <div class="hobby">Internet of things (IoT)</div>
+              <div class="hobby">Cryptocurrency</div>
+                <div class="hobby">Flying Drones</div>
+                <div class="hobby">Logistics</div>
+                <div class="hobby">Cryptocurrency</div>
+               <div class="hobby">Chess</div>
+               <div class="hobby">News</div>
+               <div class="hobby">Traveling</div>
+               <div class="hobby">Trail Blazing</div>
+
+            </div>
+          </div>
+        </div>
+
+
+      </div><!-- /#content -->
+    </div>
+</div><!-- /.page height container fix-->
+
+
+  </div><!-- /.row -->
+</div><!-- /.container -->
+
+<?php get_template_part('includes/footer'); ?>
---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+// Skills Bar
+jQuery(document).ready(function($){
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip({trigger: 'manual'}).tooltip("show")
+});
+
+// $( window ).scroll(function() {
+ // if($( window ).scrollTop() > 10){  // scroll down abit and get the action
+  $(".progress-bar").each(function(){
+    each_bar_width = $(this).attr('aria-valuenow');
+    $(this).width(each_bar_width + '%');
+  });
+}); 
+
+ //  }
+// });
+
+// Skills Bar End
